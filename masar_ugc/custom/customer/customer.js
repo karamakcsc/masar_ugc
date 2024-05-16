e40941354022072867c1f48e00ab7d17c97cf3a3
karamakcsc <karamakcsc@hotmail.com>
2024-05-16 15:11:56 +0300

    update1

--- masar_ugc/custom/customer/customer.js
+++ masar_ugc/custom/customer/customer.js
@@ -2,20 +2,21 @@
 
 frappe.ui.form.on('Customer', {
     setup: function (frm) {
-            cur_frm.fields_dict['custom_shop_evaluation'].get_query = function(doc) {
-                return {
-                    filters: {
-                        // "report_type": "Profit and Loss",
-                        "is_enable": 1,
-                    }
-                };
+        frm.fields_dict['custom_shop_evaluation'].get_query = function(doc) {
+            return {
+                filters: {
+                    // "report_type": "Profit and Loss",
+                    "is_enable": 1
+                }
             };
+        };
     }
 });
 
+
 frappe.ui.form.on('Customer', {
     setup: function (frm) {
-            cur_frm.fields_dict['custom_shop_sector'].get_query = function(doc) {
+            frm.fields_dict['custom_shop_sector'].get_query = function(doc) {
                 return {
                     filters: {
                         // "report_type": "Profit and Loss",
@@ -27,7 +28,7 @@ frappe.ui.form.on('Customer', {
 });
 frappe.ui.form.on('Customer', {
     setup: function (frm) {
-            cur_frm.fields_dict['custom_signboard_type'].get_query = function(doc) {
+            frm.fields_dict['custom_signboard_type'].get_query = function(doc) {
                 return {
                     filters: {
                         // "report_type": "Profit and Loss",
