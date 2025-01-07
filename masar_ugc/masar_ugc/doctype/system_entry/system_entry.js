ab630c448eaf0f50284bbf7d7396f03aa6dbcb32
karamakcsc <karamakcsc@hotmail.com>
2025-01-07 11:28:36 +0300

    update

--- masar_ugc/masar_ugc/doctype/system_entry/system_entry.js
+++ masar_ugc/masar_ugc/doctype/system_entry/system_entry.js
@@ -4,21 +4,23 @@
 frappe.ui.form.on("System Entry", {
     system_brand: function(frm){
         auto_number_sys_no(frm);
-    }
+    },
 });
 
 
 function auto_number_sys_no(frm) {
-    if (frm.doc.system_brand){
-        if (frm.doc.system_brand === "Uniguard") {
-            frm.doc.system_no = 1100;
-        } else if (frm.doc.system_brand === "Jotun") {
-            frm.doc.system_no = 2200;
-        } else if (frm.doc.system_brand === "Jotun&Uniguard") {
-            frm.doc.system_no = 3000;
-        } else {
-            frm.doc.system_no = null;
-        }
+    if (frm.doc.system_brand) {
+        frappe.call({
+            doc: frm.doc,
+            method: "get_max_system_no",
+            callback: function (r) {
+                if (r.message) {
+                    frm.set_value("system_no", r.message);
+                } else {
+                    frm.doc.system_no = null;
+                }
+            }
+        });
         frm.refresh_field("system_no");
     }
 }
--- masar_ugc/masar_ugc/doctype/system_entry/system_entry.py
+++ masar_ugc/masar_ugc/doctype/system_entry/system_entry.py
@@ -1,9 +1,29 @@
 # Copyright (c) 2024, KCSC and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class SystemEntry(Document):
-	pass
+    @frappe.whitelist()
+    def get_max_system_no(self):
+        if self.system_brand:
+            max_no_sql = frappe.db.sql(
+                """
+					SELECT MAX(tse.system_no) 
+					FROM `tabSystem Entry` tse 
+					WHERE  tse.system_brand = %s
+				""", (self.system_brand))
+            if not max_no_sql or not max_no_sql[0] or not max_no_sql[0][0]:
+                if self.system_brand == 'Jotun':
+                    return 2201  # 2200+1
+                elif self.system_brand == 'Uniguard':
+                    return 1101  # 1100+1
+                elif self.system_brand == 'Jotun&Uniguard':
+                    return 3001  # 3000+1
+            else:
+                max_no = max_no_sql[0][0]
+                return max_no + 1
+
+        return None
