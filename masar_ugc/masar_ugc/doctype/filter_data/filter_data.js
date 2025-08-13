5b30a889edd975b0b4c426a6d159022113e263b1
karamakcsc <karamakcsc@hotmail.com>
2025-08-13 12:00:15 +0300

    fiter data

--- masar_ugc/masar_ugc/doctype/filter_data/filter_data.js
+++ masar_ugc/masar_ugc/doctype/filter_data/filter_data.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, KCSC and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Filtered Data", {
+// frappe.ui.form.on("Filter Data", {
 // 	refresh(frm) {
 
 // 	},
--- masar_ugc/masar_ugc/doctype/filter_data/test_filter_data.py
+++ masar_ugc/masar_ugc/doctype/filter_data/test_filter_data.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestFilteredData(FrappeTestCase):
+class TestFilterData(FrappeTestCase):
 	pass
